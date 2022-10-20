07e847704ac10096e97144826e798afd0e3e2e43
Kamlesh Pokiya <kamleshpokiya1993@gmail.com>
2022-10-20 13:11:22 +0530

    blade files changes

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -33,40 +33,71 @@
     <!-- Main content -->
     <section class="content">
         <div class="row">
+
             <div class="col-md-12">
-                <div class="card card-primary">
-                    <div class="card-header">
-                        <h3 class="card-title">General</h3>
+                <div class="card card-info">
+                    <!-- <div class="card-header">
+                <h3 class="card-title">Horizontal Form</h3>
+              </div> -->
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form class="form-horizontal">
+                        <div class="card-body">
+                            <div class="form-group row">
+                                <label for="article_name" class="col-sm-2 col-form-label">Article Name</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="article_name" class="form-control" id="article_name" placeholder="Atricle Name">
+                                </div>
+                            </div>
 
-                        <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-                                <i class="fas fa-minus"></i></button>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="inputName">Article Name</label>
-                            <input type="text" id="inputName" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="inputDescription">Article Description</label>
-                            <textarea class="textarea" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                        </div>
+                            <div class="form-group row">
+                                <label for="tag" class="col-sm-2 col-form-label">Tag Name</label>
+                                <div class="col-sm-8">
+                                    <select class="form-control" name="tag_id" id="tag_id" multiple>
+                                        <option>option 1</option>
+                                        <option>option 2</option>
+                                        <option>option 3</option>
+                                        <option>option 4</option>
+                                        <option>option 5</option>
+                                    </select>
+                                </div>
+                            </div>
 
+                            <div class="form-group row">
+                                <label for="file" class="col-sm-2 col-form-label">Images</label>
+                                <div class="col-sm-8">
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="file" name="file">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                        </div>
 
-                    </div>
-                    <!-- /.card-body -->
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="inputPassword3" class="col-sm-2 col-form-label">Article Description</label>
+                                <div class="col-sm-8">
+                                    <textarea class="textarea" name="description" id="description" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="col-sm-12">
+                            <div class="card-footer">
+                                <input type="submit" value="Create new Article" class="btn btn-success">
+                                <a href="#" class="btn btn-secondary">Cancel</a>
+                            </div>
+                        </div>
+                        <!-- /.card-footer -->
+                    </form>
                 </div>
-                <!-- /.card -->
             </div>
 
         </div>
-        <div class="row">
-            <div class="col-12">
-                <a href="#" class="btn btn-secondary">Cancel</a>
-                <input type="submit" value="Create new Porject" class="btn btn-success float-right">
-            </div>
-        </div>
+
     </section>
     <!-- /.content -->
 </div>
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,4 +1,5 @@
-@extends('layout')
+@extends('partials.master')
+
 @section('content')
 <style>
     .container {
@@ -9,41 +10,103 @@
         margin-top: 50px;
     }
 </style>
-<div class="card push-top">
-    <div class="card-header">
-        Edit & Update
-    </div>
-    <div class="card-body">
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div><br />
-        @endif
-        <form method="post" action="{{ route('students.update', $student->id) }}">
-            <div class="form-group">
-                @csrf
-                @method('PATCH')
-                <label for="name">Name</label>
-                <input type="text" class="form-control" name="name" value="{{ $student->name }}" />
+
+<link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.css')}}">
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Article Edit</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Article Edit</li>
+                    </ol>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="text" class="form-control" name="email" value="{{ $student->email }}" />
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+
+            <div class="col-md-12">
+                <div class="card card-info">
+                    <!-- <div class="card-header">
+                <h3 class="card-title">Horizontal Form</h3>
+              </div> -->
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form class="form-horizontal">
+                        <div class="card-body">
+                            <div class="form-group row">
+                                <label for="article_name" class="col-sm-2 col-form-label">Article Name</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="article_name" class="form-control" id="article_name" placeholder="Atricle Name">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="tag" class="col-sm-2 col-form-label">Tag Name</label>
+                                <div class="col-sm-8">
+                                    <select class="form-control" name="tag_id" id="tag_id" multiple>
+                                        <option>option 1</option>
+                                        <option>option 2</option>
+                                        <option>option 3</option>
+                                        <option>option 4</option>
+                                        <option>option 5</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="file" class="col-sm-2 col-form-label">Images</label>
+                                <div class="col-sm-8">
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="file" name="file">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="inputPassword3" class="col-sm-2 col-form-label">Article Description</label>
+                                <div class="col-sm-8">
+                                    <textarea class="textarea" name="description" id="description" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="col-sm-12">
+                            <div class="card-footer">
+                                <input type="submit" value="Create new Article" class="btn btn-success">
+                                <a href="#" class="btn btn-secondary">Cancel</a>
+                            </div>
+                        </div>
+                        <!-- /.card-footer -->
+                    </form>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="phone">Phone</label>
-                <input type="text" class="form-control" name="phone" value="{{ $student->phone }}" />
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="text" class="form-control" name="password" value="{{ $student->password }}" />
-            </div>
-            <button type="submit" class="btn btn-block btn-danger">Update User</button>
-        </form>
-    </div>
+
+        </div>
+
+    </section>
+    <!-- /.content -->
 </div>
+<script src="{{ asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script>
+    $(function() {
+        // Summernote
+        $('.textarea').summernote()
+    })
+</script>
+<!-- /.content-wrapper -->
 @endsection
